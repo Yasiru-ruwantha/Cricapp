a08c98b449776c8b9bfc0902ecbdcb071049ed54
Yasiru-ruwantha <yasiruruwantha2652@gmail.com>
2026-09-10 20:31:34 +0530

    Add baackend structure

---
+++ cricapp-backend/src/test/java/com/crickapp/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crickapp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
